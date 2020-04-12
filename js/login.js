17bf5f609313f9948697cb95d1179837cbc05254
sunman hwang <sooon1001@gmail.com>
2020-04-12 17:00:19 +0900

    login을 sign in으로 변경 sign up생성

--- js/login.js
+++ js/login.js
@@ -1,4 +1,12 @@
-document.querySelector(".login_button").addEventListener("click", function(e) {
-  e.preventDefault();
-  alert("hi");
-});
+document
+  .querySelector(".sign_in_button")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    alert("hi");
+  });
+document
+  .querySelector(".sign_up_button")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    alert("welcome");
+  });
