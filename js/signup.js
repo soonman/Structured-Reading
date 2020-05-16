5588f029a248f69ffec540d0d4bfd957ff8a599f
soonman <soooon1001@gmail.com>
2020-05-16 23:10:45 +0900

    signup.html -Structured Reading서버에 사용자인증 가능 list.html -책 목록 나오게끔 생성

---
+++ js/signup.js
@@ -0,0 +1,28 @@
+firebase.initializeApp(firebaseConfig);
+
+document
+  .querySelector(".signup_button")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    // window.open("list.html");
+
+    var email = document.querySelector(".email_value").value;
+    var password = document.querySelector(".password_value").value;
+
+    // Promise
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(function (response) {
+        console.log(response);
+        window.location.href = "./list.html";
+      })
+      .catch(function (error) {
+        console.log("TCL: error", error);
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        alert(errorMessage);
+        // ...
+      });
+  });
